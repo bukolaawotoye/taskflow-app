9cffd062a83441fe0fda069a248103556a379e87
bukolaawotoye <135877516+bukolaawotoye@users.noreply.github.com>
2025-03-14 11:41:58 -0500

    Update main.py

--- task-flow/main.py
+++ task-flow/main.py
@@ -1,1 +1,12 @@
+#Importing Libraries
+from tkinter import *
+import sqlite3 
 
+
+root = Tk()
+root.title('Taskflow App')
+
+#Create the connector
+dataConnector = sqlite3.connect('userData.db')
+
+root.mainloop()
